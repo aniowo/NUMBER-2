c4311e5c95f7efc8e07d93d33674dd142b5ec3fd
aniowo <73297528+aniowo@users.noreply.github.com>
2020-11-10 08:28:34 +0100

    Add files via upload

---
+++ NUMBER 2/NUMBER 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUMBER_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new string[9]
+            {
+                "CALIFORNIA", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "NAIROBI",
+                "ZURICH", "LONDON", "PARIS", "ROME"
+            };
+
+            var OrderedList = array.OrderBy(s => s.Length).ThenBy(x => x).ToList();
+
+            foreach(var y in OrderedList)
+            {
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
